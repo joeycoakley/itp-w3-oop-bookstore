18427f61fe06837d267be4d9dacf5f08faac23cf
Joey <joey.coakley@gmail.com>
2017-05-12 01:52:25 +0000

    Bookstore OOP

--- bookstore/.~c9_invoke_yW8dAf.py
+++ bookstore/.~c9_invoke_yW8dAf.py
@@ -14,17 +14,15 @@ class Bookstore(object):
         if title is None and author is None:
             return "You must construct additional Pylons"
         if title is not None and author is None:
-            title_book_list = []
             for book in self.books:
-                if title.lower() in book.title.lower():
-                    title_book_list.append(book)
-            return title_book_list
+                if book.title == title:
+                    return book
         elif title is None and author is not None:
             book_list = []
-            for book in self.books:
-                if book.author.name == author.name:
-                    book_list.append(book)
-            return book_list
+            for book in author.authored_books:
+                if book.author == author.:
+                    book_list.append(book.title)
+            return book    
         elif title is not None and author is not None:
             for book in self.books:
                 if book.author == author and book.title == title:
@@ -48,14 +46,14 @@ class Author(object):
     def get_author_name(self):
         return self.name
         
-    def add_to_author_list(self, book):
-        return self.authored_books.append(book)
+    def add_to_author_list(self, title):
+        return self.authored_books.append(title)
         
 
 class Book(object):
     def __init__(self, title, author):
         self.title = title
-        self.author = author
-        author.add_to_author_list(self)
+        self.author = author.name
+        author.add_to_author_list(title)
         
         
